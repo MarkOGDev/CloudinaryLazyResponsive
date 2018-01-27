991662a31411356262e504497a5680fedb836045
MarkOGDev <Mark@broadshot.com>
2018-01-27 04:04:59 +0000

    Rename proj in package.json to og-....

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,7 @@
 
 import { LazyResponsiveImages } from './cloudinary-lazy-responsive-images'       //import like this to force render of js without actually needing to create and use the class
 import * as debounce from 'throttle-debounce/debounce';
-
+ 
 
 
 //Load and Run Lazy Repsonive
