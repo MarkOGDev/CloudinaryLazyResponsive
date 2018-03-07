5ef0b6b26ea2c0f4c3d8fe5678037447d4e6e336
MarkOGDev <Mark@broadshot.com>
2018-03-07 19:19:46 +0000

    Update to Webbpack v4

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,42 +1,39 @@
 ﻿"use strict";
 
 //const webpack = require('webpack');
-//Upgarade uglyfy when using webpack v4/ v3 has built in version
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
 const path = require('path');
-const IS_DEV = process.env.NODE_ENV === 'development';  //visual studio uses vaiable called 'NODE_ENV'
-const IS_PROD = process.env.NODE_ENV === 'production';
+
+
+//We could pass env info via npm script command.
+//const IS_DEV = process.env.NODE_ENV === 'development';   
+//const IS_PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: {
-        bundle: [path.resolve('./src/main.ts')]
-        , demo: [path.resolve('./src/demo.ts')]
-        , cloudinaryLazyResponsiveImages: [path.resolve('./src/cloudinary-lazy-responsive-images.ts')]
-        , cloudinaryLazyResponsiveImagesScrollingContainers: [path.resolve('./src/cloudinary-lazy-responsive-images-scrolling-containers.ts')]
-       // , cloudinaryLazyResponsiveImagesFixedBgScrollReveal: [path.resolve('./src/cloudinary-lazy-responsive-images-fixedbg-scroll-reveal.ts')]
-        
 
-        // "./src/main.ts"
-    }
-    , output: {
-        filename: './dist/[name]/[name].' + (IS_PROD ? 'min.' : '') + 'js',
-        // filename: "./dist/bundle.js",
-        // libraryTarget: 'var',
+    watch: true,
+  //  entry: {
+      //  bundle: [path.resolve('./src/main.ts')]
+     //   , demo: [path.resolve('./src/demo.ts')]
+     //   , cloudinaryLazyResponsiveImages: [path.resolve('./src/cloudinary-lazy-responsive-images.ts')]
+     //   , cloudinaryLazyResponsiveImagesScrollingContainers: [path.resolve('./src/cloudinary-lazy-responsive-images-scrolling-containers.ts')]
+       // , cloudinaryLazyResponsiveImagesFixedBgScrollReveal: [path.resolve('./src/cloudinary-lazy-responsive-images-fixedbg-scroll-reveal.ts')]       
+   // },
+    output: {
+      //  filename: './dist/[name]/[name].' + (IS_PROD ? 'min.' : '') + 'js',
+
+
         libraryTarget: 'umd',
-        library: ['clri', '[name]']
+        library: 'clri'
     },
-    devServer: {
-        contentBase: ".",
-        host: "localhost",
-        port: 9000
-    },
+  //  devServer: {
+   //     contentBase: ".",
+   //     host: "localhost",
+   //     port: 9000
+   // },
     module: {
         rules: [{
-            test: /\.ts$/,
-      
-            // exclude: /node_modules/,
-
+            test: /\.ts$/,      
+            exclude: /node_modules/,
             use: [{
                 loader: 'babel-loader',
                 options: {
